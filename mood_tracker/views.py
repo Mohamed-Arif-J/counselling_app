eb2395bc9d4a594a35a9dbcdadf255269b19bd01
roshanroy-lang <roshanzion2006@gmail.com>
2026-06-04 16:36:31 +0530

    Intern5 Day1

--- mood_tracker/views.py
+++ mood_tracker/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+
 
 # Create your views here.
+from django.shortcuts import render, redirect
+from .forms import MoodLogForm
+from .models import MoodLog
+from django.contrib.auth.decorators import login_required
+
+@login_required
+def mood_checkin(request):
+    if request.method == "POST":
+        form = MoodLogForm(request.POST)
+        if form.is_valid():
+            mood_log = form.save(commit=False)
+            mood_log.user = request.user
+            mood_log.save()
+            return redirect('mood_history')
+    else:
+        form = MoodLogForm()
+    return render(request, 'mood/checkin.html', {'form': form})
+
+@login_required
+def mood_history(request):
+    logs = MoodLog.objects.filter(user=request.user).order_by('-created_at')
+    return render(request, 'mood/history.html', {'logs': logs})
+
