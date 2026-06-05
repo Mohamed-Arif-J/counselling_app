df5d57b9d5878afb63c6aeec8b7684e8800b448c
roshanroy-lang <roshanzion2006@gmail.com>
2026-06-05 16:10:40 +0530

    Day3 Journal Admin Registered

--- mood_tracker/admin.py
+++ mood_tracker/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import MoodLog
+from .models import JournalEntry, MoodLog
 
 # Register your models here.
 admin.site.register(MoodLog)
+admin.site.register(JournalEntry)
