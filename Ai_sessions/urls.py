628cd3b35fa629cabd6a1ae041c6588a24f216d0
Arif <mohmedarifj@gmail.com>
2026-06-06 13:15:10 +0530

    Complete Day 3 AI modules

--- Ai_sessions/urls.py
+++ Ai_sessions/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import chatbot, sentiment
+from .views import chatbot, sentiment, crisis
+
 urlpatterns = [
     path("chat/", chatbot),
     path("sentiment/", sentiment),
+    path("crisis/", crisis),
 ]
