baaeec7e32dc8d81f3f22d1caf0d67a413ef2bd1
Akshay-Menon <akshaymenon315@gmail.com>
2026-06-06 12:32:40 +0530

    manually added clean appointments feature files

--- appointments/admin.py
+++ appointments/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Appointment,SessionNote
+from .models import Appointment, SessionNote
 
 # Register your models here.
 admin.site.register(Appointment)
--- appointments/urls.py
+++ appointments/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
-from . import views
+from .views import list_appointments, available, book, cancel_appointment, session_notes, confirm_appointment, send_mails, cancel_appointment_email
 
 urlpatterns = [
-    path('', views.list_appointments, name='list_appointments'),
-    path('therapists/', views.available, name='available'),
-    path('book/', views.book, name='book'),
-    path('cancel/<int:appointment_id>/', views.cancel_appointment, name='cancel_appointment'),
-    # path('notes/<int:appointment_id>/', views.manage_session_notes, name='manage_session_notes'),
+    path("", list_appointments, name="list_appointments"),
+    path("therapists/", available, name="available"),
+    path("book/", book, name="book"),
+    path("cancel/<int:appointment_id>/", cancel_appointment, name="cancel_appointment"),
+    path("cancel_email/<int:appointment_id>/", cancel_appointment_email, name="cancel_appointment_email"),
+    path("notes/<int:appointment_id>/", session_notes, name="session_notes"),
+    path("confirm/<int:appointment_id>/", confirm_appointment, name="confirm_appointment"),
+    path("send_mails/", send_mails, name="send_mails")
 ]
