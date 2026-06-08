fab9e1d07fd364752815b703186de40530b77c0d
roshanroy-lang <roshanzion2006@gmail.com>
2026-06-08 16:40:20 +0530

    Thumbnail Feature In Article Section Added.

--- mood_tracker/models.py
+++ mood_tracker/models.py
@@ -53,6 +53,7 @@ class PsychoeducationArticle(models.Model):
     category = models.CharField(max_length=50, choices=CATEGORY_CHOICES, default="cbt")
     video_url = models.URLField(blank=True, null=True)   # optional video link
     exercise = models.TextField(blank=True, null=True)   # optional exercise instructions
+    thumbnail = models.ImageField(upload_to="article_thumbnails/", blank=True, null=True) 
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
